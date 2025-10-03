4393aa06dcf53120e5cb56b5a25001eeab1bf9eb
Robert_hubskyi <burdan1337@i.ua>
2025-10-03 17:25:02 +0300

    Fix PATCH /user/status 401 error by updating authorization handling in Swagger

--- core/src/main/java/greencity/config/SecurityConfig.java
+++ core/src/main/java/greencity/config/SecurityConfig.java
@@ -25,17 +25,12 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import java.util.Arrays;
 import java.util.Collections;
+
 import static greencity.constant.AppConstant.*;
 import static jakarta.servlet.http.HttpServletResponse.SC_FORBIDDEN;
 import static jakarta.servlet.http.HttpServletResponse.SC_UNAUTHORIZED;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
-/**
- * Config for security.
- *
- * @author Nazar Stasyuk && Yurii Koval
- * @version 1.0
- */
 @Configuration
 @EnableWebSecurity
 @EnableGlobalAuthentication
@@ -45,10 +40,6 @@ public class SecurityConfig {
     private static final String USER_LINK = "/user";
     private final AuthenticationConfiguration authenticationConfiguration;
 
-    /**
-     * Constructor.
-     */
-
     @Autowired
     public SecurityConfig(JwtTool jwtTool, UserService userService,
                           AuthenticationConfiguration authenticationConfiguration) {
@@ -57,35 +48,23 @@ public class SecurityConfig {
         this.authenticationConfiguration = authenticationConfiguration;
     }
 
-    /**
-     * Bean {@link PasswordEncoder} that uses in coding password.
-     */
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    /**
-     * Method for configure security.
-     *
-     * @param http {@link HttpSecurity}
-     */
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.cors(corsCustomizer -> corsCustomizer.configurationSource(request -> {
-            CorsConfiguration config = new CorsConfiguration();
-            config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
-            config.setAllowedOrigins(Collections.singletonList("http://localhost:4205"));
-            config.setAllowedMethods(
-                    Arrays.asList("GET", "POST", "OPTIONS", "DELETE", "PUT", "PATCH"));
-            config.setAllowedHeaders(
-                    Arrays.asList("Access-Control-Allow-Origin", "Access-Control-Allow-Headers",
-                            "X-Requested-With", "Origin", "Content-Type", "Accept", "Authorization"));
-            config.setAllowCredentials(true);
-            config.setAllowedHeaders(Collections.singletonList("*"));
-            config.setMaxAge(3600L);
-            return config;
-        }))
+                    CorsConfiguration config = new CorsConfiguration();
+                    config.setAllowedOrigins(Arrays.asList("http://localhost:4200", "http://localhost:4205"));
+                    config.setAllowedMethods(
+                            Arrays.asList("GET", "POST", "OPTIONS", "DELETE", "PUT", "PATCH"));
+                    config.setAllowedHeaders(Collections.singletonList("*"));
+                    config.setAllowCredentials(true);
+                    config.setMaxAge(3600L);
+                    return config;
+                }))
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
                 .addFilterBefore(
@@ -97,26 +76,33 @@ public class SecurityConfig {
                         .accessDeniedHandler((req, resp, exc) -> resp.sendError(
                                 SC_FORBIDDEN, "You don't have authorities.")))
                 .authorizeHttpRequests(req -> req
-                        .requestMatchers("/static/css/**", "/static/img/**").permitAll()
-                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+
+
+                        .requestMatchers(HttpMethod.POST, "/ownSecurity/signIn").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/ownSecurity/signUp").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/ownSecurity/updatePassword").permitAll()
+
+
                         .requestMatchers(
                                 "/v2/api-docs/**",
                                 "/v3/api-docs/**",
                                 "/swagger.json",
-                                "/swagger-ui.html")
-                        .permitAll()
-                        .requestMatchers(
+                                "/swagger-ui.html",
                                 "/swagger-resources/**",
                                 "/webjars/**",
                                 "/swagger-ui/**")
                         .permitAll()
+
+
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers(HttpMethod.GET,
                                 "/ownSecurity/verifyEmail",
                                 "/ownSecurity/updateAccessToken",
                                 "/ownSecurity/restorePassword",
                                 "/googleSecurity",
                                 "/facebookSecurity/generateFacebookAuthorizeURL",
-                                "/facebookSecurity/facebook", "/user/emailNotifications",
+                                "/facebookSecurity/facebook",
+                                "/user/emailNotifications",
                                 "/user/activatedUsersAmount",
                                 "/user/{userId}/habit/assign",
                                 "/token",
@@ -125,15 +111,15 @@ public class SecurityConfig {
                                 "/user/checkByUuid",
                                 "/user/get-user-rating")
                         .permitAll()
-                        .requestMatchers(HttpMethod.POST,
-                                "/ownSecurity/signUp",
-                                "/ownSecurity/signIn",
-                                "/ownSecurity/updatePassword")
-                        .permitAll()
+
+
+                        .requestMatchers(HttpMethod.POST, "/user/filter").hasRole("ADMIN")
+
                         .requestMatchers(HttpMethod.GET, USER_LINK,
                                 "/user/shopping-list-items/habits/{habitId}/shopping-list",
                                 "/user/{userId}/{habitId}/custom-shopping-list-items/available",
-                                "/user/{userId}/profile/", "/user/isOnline/{userId}/",
+                                "/user/{userId}/profile/",
+                                "/user/isOnline/{userId}/",
                                 "/user/{userId}/profileStatistics/",
                                 "/user/userAndSixFriendsWithOnlineStatus",
                                 "/user/userAndAllFriendsWithOnlineStatus",
@@ -152,6 +138,7 @@ public class SecurityConfig {
                                 "/ownSecurity/password-status",
                                 "/user/emailNotifications")
                         .hasAnyRole(USER, ADMIN, UBS_EMPLOYEE, MODERATOR, EMPLOYEE)
+
                         .requestMatchers(HttpMethod.POST, USER_LINK,
                                 "/user/shopping-list-items",
                                 "/user/{userId}/habit",
@@ -162,6 +149,7 @@ public class SecurityConfig {
                                 "/email/changePlaceStatus",
                                 "/email/general/notification")
                         .hasAnyRole(USER, ADMIN, UBS_EMPLOYEE, MODERATOR, EMPLOYEE)
+
                         .requestMatchers(HttpMethod.PUT,
                                 "/ownSecurity/changePassword",
                                 "/user/profile",
@@ -169,6 +157,7 @@ public class SecurityConfig {
                                 "/user/language/{languageId}",
                                 "/user/employee-email")
                         .hasAnyRole(USER, ADMIN, UBS_EMPLOYEE, MODERATOR, EMPLOYEE)
+
                         .requestMatchers(HttpMethod.PUT,
                                 "/user/edit-authorities",
                                 "/user/authorities",
@@ -176,20 +165,24 @@ public class SecurityConfig {
                                 "/user/markUserAsDeactivated",
                                 "/user/markUserAsActivated")
                         .hasAnyRole(ADMIN, UBS_EMPLOYEE, MODERATOR, EMPLOYEE)
+
                         .requestMatchers(HttpMethod.GET,
                                 "/user/get-all-authorities",
                                 "/user/get-positions-authorities",
                                 "/user/get-employee-login-positions")
                         .hasAnyRole(ADMIN, UBS_EMPLOYEE, MODERATOR, EMPLOYEE)
+
                         .requestMatchers(HttpMethod.PATCH,
                                 "/user/shopping-list-items/{userShoppingListItemId}",
                                 "/user/profilePicture",
                                 "/user/deleteProfilePicture")
                         .hasAnyRole(USER, ADMIN, UBS_EMPLOYEE, MODERATOR, EMPLOYEE)
+
                         .requestMatchers(HttpMethod.DELETE,
                                 "/user/shopping-list-items/user-shopping-list-items",
                                 "/user/shopping-list-items")
                         .hasAnyRole(USER, ADMIN, UBS_EMPLOYEE, MODERATOR, EMPLOYEE)
+
                         .requestMatchers(HttpMethod.GET,
                                 "/user/all",
                                 "/user/roles",
@@ -197,54 +190,41 @@ public class SecurityConfig {
                                 "/user/searchBy",
                                 "/user/findAll")
                         .hasAnyRole(ADMIN, MODERATOR, EMPLOYEE)
-                        .requestMatchers(HttpMethod.POST,
-                                "/ownSecurity/sign-up-employee")
+
+                        .requestMatchers(HttpMethod.POST, "/ownSecurity/sign-up-employee")
                         .hasAnyRole(UBS_EMPLOYEE)
-                        .requestMatchers(HttpMethod.POST,
-                                "/user/filter",
-                                "/ownSecurity/register")
-                        .hasAnyRole(ADMIN)
+
                         .requestMatchers(HttpMethod.PATCH,
                                 "/user/status",
                                 "/user/role",
                                 "/user/update/role")
-                        .hasAnyRole(ADMIN)
+                        .hasAuthority("ROLE_ADMIN")
+
                         .requestMatchers(HttpMethod.POST, "/management/login")
-                        // .not().fullyAuthenticated()
-                        .rememberMe()
-                        .requestMatchers(HttpMethod.GET, "/management/login")
-                        .permitAll()
-                        .requestMatchers("/css/**", "/img/**")
-                        .permitAll()
+                        .hasRole("ADMIN")
+
+                        .requestMatchers(HttpMethod.GET, "/management/login").permitAll()
+                        .requestMatchers("/css/**", "/img/**").permitAll()
+
                         .requestMatchers(HttpMethod.PUT, "/user/user-rating")
                         .hasAnyRole(ADMIN, MODERATOR, EMPLOYEE, UBS_EMPLOYEE, USER)
-                        .anyRequest().hasAnyRole(ADMIN));
+
+
+                        .anyRequest().authenticated()
+                );
         return http.build();
     }
 
-    /**
-     * Method for configure type of authentication provider.
-     *
-     * @param auth {@link AuthenticationManagerBuilder}
-     */
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) {
         auth.authenticationProvider(new JwtAuthenticationProvider(jwtTool));
     }
 
-    /**
-     * Provides AuthenticationManager.
-     *
-     * @return {@link AuthenticationManager}
-     */
     @Bean
     public AuthenticationManager authenticationManager() throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
 
-    /**
-     * Bean {@link GoogleIdTokenVerifier} that uses in verify googleIdToken.
-     */
     @Bean
     public GoogleIdTokenVerifier googleIdTokenVerifier() {
         return new GoogleIdTokenVerifier.Builder(new NetHttpTransport(),
